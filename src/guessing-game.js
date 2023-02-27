b73c3468c7161809028158bfea894853ae07f0b3
Victor Shevchenko <13shev.js@gmail.com>
2023-02-27 03:54:06 +0300

    feat: task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    this.count = 0;
+    console.log(this);
+  }
 
-    }
+  guess() {
+    this.result = Math.round((this.max - this.min) / 2 + this.min);
+    this.count++;
+    console.log(this.count, 'result = ', this.result);
+    return this.result;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.result;
+    console.log('lower, max = ', this.max);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.result;
+    console.log('greater, min = ', this.min);
+  }
 }
 
 module.exports = GuessingGame;
